d76f4e560cb74cdcffe5073bdb6d91d9d803c72a
Andrew Smith <asmith@bcm.com.au>
2012-11-02 13:34:34 +1000

    Correct copy/links for the store informaiton

--- pages/poster.php
+++ pages/poster.php
@@ -8,7 +8,7 @@
 </nav>
 <h3><?php echo $json['poster']['head-sub']; ?></h3>
 <figure title="Infographic poster example">
-	<a href="http://yourlogicalfallacy.myshopify.com" title="Buy your high quality printed wall poster" target="_blank">
+	<a href="http://store.yourlogicalfallacyis.com" title="Buy your high quality printed wall poster" target="_blank">
 		<img src="assets/poster-new.jpg" width="726" height="492" alt="Logical Fallacies Poster" />
 		<span class="buy-now"></span>
 	</a>
@@ -17,7 +17,7 @@
 <!--article><?php //echo $json['poster']['description']; ?></article-->
 <article>
 <p>After launching this site we realised that it was costing people $60-$100 to have the posters individually printed, but by printing in bulk we could make it a lot cheaper.</p>
-<p>So, we’ve set up a shop on <a href="http://yourlogicalfallacy.myshopify.com" target="_blank" title="Buy via Shopify.com">shopify.com</a> where you can order a <a class="preview" href="#" data-type="sml">16x24”</a> or <a class="preview" href="#"  data-type="lrg">24x36”</a> poster printed on high quality satin paper stock delivered to your door in the mainland USA for a flat rate of USD$20 including shipping and tax - extra for international orders. Rest assured that the money we make from it will go to a good cause (beer and/or education projects).</p>
+<p>We've set up a <a href="http://store.yourlogicalfallacyis.com" target="_blank" title="Buy via Shopify.com">shop</a> where you can order a <a class="preview" href="#" data-type="sml">16x24”</a> or <a class="preview" href="#"  data-type="lrg">24x36”</a> poster printed on high quality satin paper stock delivered to your door in the mainland USA for a flat rate of USD$20 including shipping and tax - extra for international orders. Rest assured that the money we make from it will go to a good cause (beer and/or education projects).</p>
 <h4>Here are some reasons you might want to order one of these pretty great posters:</h4>
 <ol>
 	<li>To hang up near your computer for when you are arguing with people on the internets.</li>
